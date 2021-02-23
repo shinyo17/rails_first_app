a1d50a3ffad2ce28b09be639b4d2dbe487cf21f2
shinyo17 <dntoskwem@gmail.com>
2021-02-23 11:31:50 +0900

    rails girls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'pages/info'
+  resources :ideas
   # get '/' => 'home#index'
-  root 'home#index'
+  # root 'home#index'
+  root to: redirect('/ideas')
 end
